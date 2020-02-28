a8248f2f3186356c336b0eecd6a205c7137683fc
Kevin Eaton <kevin.eaton@mymail.champlain.edu>
2020-02-28 10:14:13 -0500

    updates

--- data/manage_data.php
+++ data/manage_data.php
@@ -39,7 +39,7 @@ function mysql_div_list($mysql_result, $id_key, $name_key, $url){
 <?php include("../include/header_2.inc"); ?>
 
 <table>
-	<tr>
+	<tr colspan="2">
 		<td id='banner'>
 			<h1>Welcome to the Client Data Portal</h1>
 		</td>
@@ -78,74 +78,10 @@ function mysql_div_list($mysql_result, $id_key, $name_key, $url){
 				<input name='data' size='5' maxlength='5'/>
 			</div>
 		</div>
-		<!--<span>Select a Process:</span>
-			<span class='radio'>
-				<input type='radio' name='p' value='d' onclick=showQuery('all_dept') />
-				Department
-			</span>
-			<span class='radio'>
-				<input type='radio' name='p' value='a'/>
-				Application Software
-			</span>
-			<span class='radio'>
-				<input type='radio' name='p' value='o' onclick=showQuery('all_ops') />
-				Operating System
-			<span class='radio'>
-				<input type='radio' name='p' value='d' onclick=showQuery('all_st') />
-				Residency: By State
-			</span>
-			<span class='radio'>
-				<input type='radio' name='p' value='d' onclick=showQuery('all_zip') />
-				Residency: By Zip Code
-			</span>-->
 	</div>
 	<div id="data"></div>
-	<form id="manage_form" name='process' action='../data/select_output.php' method='post'>
-	</form>
-<!--<form name='process' action='../data/select_output.php' method='post'>
-<div id='all_dept'>
-	Select A Department:
-		<?php mysql_selector($result1, 'dept_id', 'dept_name'); ?>
-	<p>
-		<input type='hidden' value='1' name='query_id'/>
-		<input type='submit' value='Select Data' name='s' />
-	</p>
-</div>
-</form>
-
-<form name='process' action='../data/select_output.php' method='post'>
-<div id='all_ops'>
-	Select An Operating System:
-		<?php mysql_selector($result2,'soft_id', 'soft_name'); ?>
-	<p>
-		<input type='hidden' value='2' name='query_id'/>
-		<input type='submit' value='Select Data' name='s' />
-	</p>
-</div>
-</form>
-
-<form name='process' action='../data/select_output.php' method='post'>
-<div id='all_st'>
-	Select A State:
-	
-	<p>
-		<input type='hidden' value='3' name='query_id'/>
-		<input type='submit' value='Select Data' name='s' />
-	</p>
-</div>
-</form>
-
-
-<form name='process' action='../data/select_output.php' method='post'>
-<div id='all_zip'>
-	Enter  Zip Code:<input name='data' size='5' maxlength='5'/>
-	<p>
-	<input type='hidden' value='4' name='query_id'/>
-		<input type='submit' value='Select Data' name='s' />
-	</p>
-</div>
-</form>-->
 </td>
+
 </tr>
 </table>
 
